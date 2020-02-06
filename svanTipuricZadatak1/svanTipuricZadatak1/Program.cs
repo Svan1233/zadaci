e915fb63ca020757984d0f5e095e1777a9aba71f
Svan1233 <55134376+Svan1233@users.noreply.github.com>
2020-02-06 10:21:41 +0100

    Add files via upload

---
+++ svanTipuricZadatak1/svanTipuricZadatak1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace svanTipuricZadatak1
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            double c;
+            double prosjek;
+            Console.WriteLine("Upisite a: " );
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Upisite b: " );
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Upisite c: " );
+            c = Convert.ToDouble(Console.ReadLine());
+
+            prosjek = (a + b + c) / 3;
+            Console.WriteLine("Prosjek je: " + prosjek);
+
+            Console.ReadKey();
+        }
+
+    }
+}
