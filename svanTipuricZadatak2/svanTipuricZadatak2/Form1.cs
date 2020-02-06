bced2c18178b0ae69b06a1e556c0323589fc7742
Svan1233 <55134376+Svan1233@users.noreply.github.com>
2020-02-06 11:01:42 +0100

    Add files via upload

---
+++ svanTipuricZadatak2/svanTipuricZadatak2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace svanTipuricZadatak2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            Console.ReadLine();
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            Console.ReadLine();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show(textBox1.Text + textBox2.Text);
+        }
+    }
+}
